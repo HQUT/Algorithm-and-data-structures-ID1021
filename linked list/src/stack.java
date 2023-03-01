3aad84b2cbc9c7ba20fd1d6020b009138481ac26
HQUT <126714611+HQUT@users.noreply.github.com>
2023-03-01 21:55:18 +0100

    Update stack.java

--- linked list/src/stack.java
+++ linked list/src/stack.java
@@ -3,8 +3,8 @@
 
         private class Node {
 
-            int value; // integer data
-            Node next; // reference variable Node type
+            int value; 
+            Node next; 
         }
 
         Node top;
@@ -12,37 +12,35 @@
         public void Stack() {
             this.top = null;
         }
-            // Utility function to add an element x in the stack
-            public void push(int x) // insert at the beginning
+           
+            public void push(int x) 
             {
-                // create new node temp and allocate memory
+              
                 Node temp = new Node();
 
-                // check if stack (heap) is full. Then inserting an
-                //  element would lead to stack overflow
+                
                 if (temp == null) {
                     System.out.print("\nHeap Overflow");
                     return;
                 }
 
-                // initialize data into temp data field
+                
                 temp.value = x;
 
-                // put top reference into temp link
+               
                 temp.next = top;
 
-                // update top reference
+               
                 top = temp;
             }
 
-            // Utility function to check if the stack is empty or
-            // not
+           
             public boolean isEmpty() { return top == null; }
 
-            // Utility function to return top element in a stack
+          
             public int peek()
             {
-                // check for empty stack
+              
                 if (!isEmpty()) {
                     return top.value;
                 }
@@ -52,22 +50,22 @@
                 }
             }
 
-            // Utility function to pop top element from the stack
-            public void pop() // remove at the beginning
+           
+            public void pop() 
             {
-                // check for stack underflow
+              
                 if (top == null) {
                     System.out.print("\nStack Underflow");
                     return;
                 }
 
-                // update the top pointer to point to the next node
+                
                 top = (top).next;
             }
 
             public void display()
             {
-                // check for stack underflow
+               
                 if (top == null) {
                     System.out.printf("\nStack Underflow");
                 }
@@ -75,10 +73,10 @@
                     Node temp = top;
                     while (temp != null) {
 
-                        // print node data
+                        
                         System.out.printf("%d->", temp.value);
 
-                        // assign temp link to temp
+                    
                         temp = temp.next;
                     }
                 }
