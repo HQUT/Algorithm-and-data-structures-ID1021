a4272384fe94546ab67360c895c570d405af270c
HQUT <126714611+HQUT@users.noreply.github.com>
2023-03-01 21:53:22 +0100

    Update LinkedList.java

--- linked list/src/LinkedList.java
+++ linked list/src/LinkedList.java
@@ -68,30 +68,28 @@ public class LinkedList {
         if (head == null)
             return;
 
-        // Store head node
+       
         Node temp = head;
 
-        // If head needs to be removed
+        
         if (index == 0) {
             head = temp.next; // Change head
             return;
         }
 
-        // Find previous node of the node to be deleted
+       
         for (int i = 0; temp != null && i < index- 1;
              i++)
             temp = temp.next;
 
-        // If position is more than number of nodes
+       
         if (temp == null || temp.next == null)
             return;
 
-        // Node temp->next is the node to be deleted
-        // Store pointer to the next of node to be deleted
+        
         Node next = temp.next.next;
 
-        temp.next
-                = next; // Unlink the deleted node from list
+        temp.next = next; 
     }
 
 
