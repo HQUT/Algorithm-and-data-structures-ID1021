4fefd03439e0af5419ea0da26faaefa02d1757aa
HQUT <126714611+HQUT@users.noreply.github.com>
2023-03-01 22:42:46 +0100

    Update main.java

--- Quick sort/src/main.java
+++ Quick sort/src/main.java
@@ -9,20 +9,12 @@ public class main {
         int low = 0;
         int high = arr.length-1;
         Linked_list list = new Linked_list();
-       // System.out.println("the sequence with random number; ");
         for(int i= 0; i< sequence.length; i++){
             sequence[i] = rnd.nextInt(50000);
         }
-       // System.out.println(Arrays.toString(sequence));
-
-       // System.out.println("\n the array before sorting; ");
         for(int i=0; i< arr.length;i++){
             arr[i]= sequence[i];
         }
-       // Quicksort.printArray(arr,arr.length);
-        //System.out.println(Arrays.toString(arr));
-
-       // System.out.println("\n the arrat efter sorting:");
         double min1 = Double.POSITIVE_INFINITY;
         for(int j=0 ; j<1000 ;j++) {
             long t2 = System.nanoTime();
@@ -34,17 +26,12 @@ public class main {
         }
         double t_arr = (min1/1000);
         double t= t_arr/(k*(Math.log(k)));
-       // Quicksort.printArray(arr,arr.length);
         System.out.println("\n Time it takes to sort the arrar: "+ t_arr);
         System.out.println("\n log or not? "+ t);
-
-       // System.out.println("\n the list before sorting: ");
         for(int i =0; i< k;i++){
             int value =sequence[i];
             list.add(value);
         }
-       // list.printList(list.head);
-
         Linked_list.Node n = list.head;
         while (n.next != null)
             n = n.next;
@@ -59,8 +46,6 @@ public class main {
         }
         double t_list = (min2/1000);
         double log= t_list/(k*(Math.log(k)));
-       // System.out.println("\nThe list efter sorting: ");
-       // list.printList(list.head);
         System.out.println("\n the time it takes to sort a list: "+t_list);
         System.out.println("\n is it log komplexity? " +log);
         System.out.println("\n k? " + t_list/t_arr);
