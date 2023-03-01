b215eee102e31addc89408f4d2d7f0648aeae508
HQUT <126714611+HQUT@users.noreply.github.com>
2023-03-01 21:45:35 +0100

    Update merge_sort.java

--- Sorted Data/src/merge_sort.java
+++ Sorted Data/src/merge_sort.java
@@ -24,33 +24,33 @@ public class merge_sort {
     }
 
     private static void merge( int[]org, int[] aux, int lo, int mid, int hi) {
-        // copy all items from lo to hi from org to aux
+        
         for ( int i=lo; i<=hi; i++ ){
             aux[i]=org[i];
         }
-// let's do the merging
-        int i = lo; // the index in the first part
-        int j = mid + 1; // the index in the second part
-        // for all indeces from lo to hi
+
+        int i = lo;
+        int j = mid + 1; 
+       
         for (int k = lo; k <=hi; k++) {
-// if i is greater than mid, move the j item to the org array, update j
+
          if(i>mid){
              org[k]=aux[j];
              j++;
          }
-// else if j is greate than hi, move the i item to the org array, update i
+
             if(j>hi){
                 org[k]=aux[i];
                 i++;
             }
-// else if the i item is smaller than the j item ,move it to the org array, update i
+
 
             if (aux[i]< aux[j]){
                 org[k]=aux[i];
                 i++;
             }
 
-// else you can move the j item to the org array, update j
+
             else{
                 org[k]=aux[j];
                j++;
@@ -75,7 +75,7 @@ public class merge_sort {
         for (int j = 0; j <= 1000; j++) {
             long t0 = System.nanoTime();
 
-            //merge(array,aux,0,mid,hi);
+            
             sort(array);
             long t1=System.nanoTime();
             t_access+=(t1-t0);
